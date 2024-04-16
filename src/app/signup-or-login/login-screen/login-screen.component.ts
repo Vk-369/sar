4b09473c03b903624fea936d7b6faf0ff55392e9
Dhana <gautham523sai@gmail.com>
2024-04-16 16:46:12 +0530

    login screen and reset password done

--- src/app/signup-or-login/login-screen/login-screen.component.ts
+++ src/app/signup-or-login/login-screen/login-screen.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from 'src/app/common/common.service';
+import { SarServiceService } from 'src/app/sar-service.service';
 
 @Component({
   selector: 'app-login-screen',
@@ -11,38 +14,164 @@ export class LoginScreenComponent {
   isGmailIdEntered: boolean = false;
   openModal: boolean = false;
   text: any = '';
-  constructor(private router: Router) {
-    
+
+  loginForm!: FormGroup;
+  blurControls: any = { mail_id: false, password: false, repassword: false };
+  fragment: any = {};
+  resetMode: boolean = false;
+  resetForm!: FormGroup;
+  inReset: boolean = false;
+  incorrectMatch: boolean = false;
+  passwordStrength: any = {
+    charLength: false,
+    specialChar: false,
+    digit: false,
+    capitalChar: false,
+  };
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    public commonService: CommonService,
+    private _sarService: SarServiceService
+  ) {}
+
+  ngOnInit() {
+    this._route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        this.fragment = this._sarService.decodeParams(fragment);
+        if (this.fragment.type === 'reset password') {
+          this.resetMode = true;
+          this.initResetForm();
+        }
+      }
+      console.log(this.fragment);
+    });
+    this.initForm();
   }
 
+  initForm() {
+    this.loginForm = new FormGroup({
+      mail_id: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  initResetForm() {
+    this.resetForm = new FormGroup({
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(60),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{4,}$'
+        ),
+      ]),
+      repassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(60),
+      ]),
+    });
+  }
+
+  passwordComplexity(event: Event) {
+    this.passwordStrength.charLength =
+      this.resetForm.get('password')?.value.length > 7;
+    this.passwordStrength.capitalChar = /[A-Z]/.test(
+      this.resetForm.get('password')?.value
+    );
+    this.passwordStrength.digit = /[0-9]/.test(
+      this.resetForm.get('password')?.value
+    );
+    this.passwordStrength.specialChar = /[@$!%*?&]/.test(
+      this.resetForm.get('password')?.value
+    );
+  }
+
+  onBlur(control: string) {
+    this.blurControls[control] = true;
+  }
+
+  gotoConfirm() {
+    if (this.resetForm.get('password')?.invalid) {
+      this.resetForm.get('password')?.markAsTouched();
+      return;
+    }
+    this.inReset = true;
+  }
+
+  resetConfirm() {
+    this.incorrectMatch = false;
+    if (this.resetForm.get('repassword')?.invalid) {
+      this.blurControls.repassword = true;
+      this.resetForm.markAllAsTouched();
+      return;
+    }
+    this.incorrectMatch = !(
+      this.resetForm.get('password')?.value ===
+      this.resetForm.get('repassword')?.value
+    );
+    if (this.incorrectMatch) {
+      this.blurControls['repassword'] = false;
+      this.resetForm.get('repassword')?.reset();
+      this.resetForm.get('repassword')?.updateValueAndValidity();
+      return;
+    }
+    this.openModal = true;
+  }
 
   resetPassword() {
     this.isResetClicked = true;
-    this.isGmailIdEntered = false;
-    console.log('this is into the reset password component');
+    this.blurControls.mail_id = false;
   }
-  gmailEnter() {
-    this.isGmailIdEntered = true;
-  }
-  verifyOtp() {
-    this.openModal = true;
-    this.text = 'qwertyuiop';
-    setTimeout(() => {
-      this.text = 'this is  the altered text bro';
-    }, 5000);
-  }
-  fromModal(e: any) {
-    if (e) {
-      console.log(e, 'this is the event getting emitted from modal component');
-      this.openModal = false;
-    }
-  }
+
   authenticate() {
+    console.log(this.loginForm.invalid);
+    if (this.loginForm.invalid) {
+      this.onBlur('mail_id');
+      this.onBlur('password');
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    this._router.navigate(['/musicPlayer']);
     console.log('into the authentication');
   }
-  navigation()
-  {
-this.router.navigate(['/musicPlayer'])
 
+  sendOtp() {
+    if (this.loginForm.get('mail_id')?.invalid) {
+      this.onBlur('mail_id');
+      return;
+    }
+    const mail = this._sarService.encodeParams({
+      mail_id: this.loginForm.get('mail_id')?.value,
+      type: 'reset password',
+    });
+    this._router.navigate(['/verify'], { fragment: mail });
+  }
+
+  back() {
+    if (this.isResetClicked) {
+      this.loginForm.reset();
+      this.isResetClicked = false;
+      return;
+    }
+    this._router.navigate(['']);
+  }
+
+  modalEvent(e: any) {
+    if (this.incorrectMatch) {
+      console.log(this.incorrectMatch);
+      this.incorrectMatch = false;
+      return;
+    }
+    console.log('navigate');
+    this._router.navigate(['']);
+  }
+
+  confirmEvent(event: boolean) {
+    this.resetMode = false;
+    this.resetForm.reset();
+    this._router.navigate(['/login']);
   }
 }
--- src/app/signup-or-login/otp/otp.component.ts
+++ src/app/signup-or-login/otp/otp.component.ts
@@ -6,59 +6,69 @@ import { SarServiceService } from 'src/app/sar-service.service';
 @Component({
   selector: 'app-otp',
   templateUrl: './otp.component.html',
-  styleUrls: ['./otp.component.scss']
+  styleUrls: ['./otp.component.scss'],
 })
 export class OtpComponent {
-  otpForm!: FormGroup
-  verifySuccess: boolean = false
-  fragment: any = {}
+  otpForm!: FormGroup;
+  verifySuccess: boolean = false;
+  fragment: any = {};
 
-  constructor(private _router: Router, private _route: ActivatedRoute, private _sarService: SarServiceService){
-  }
+  modalButton = 'Login';
 
-  ngOnInit(){
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _sarService: SarServiceService
+  ) {}
+
+  ngOnInit() {
     this._route.fragment.subscribe((fragment) => {
       if (fragment) {
-        this.fragment = this._sarService.decodeParams(fragment)
+        this.fragment = this._sarService.decodeParams(fragment);
+        if (this.fragment.type === 'reset password') {
+          this.modalButton = 'Set password';
+        }
       }
-    })
-    this.initForm()
+    });
+    this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.otpForm = new FormGroup({
       digit1: new FormControl(null, Validators.required),
       digit2: new FormControl(null, Validators.required),
       digit3: new FormControl(null, Validators.required),
-      digit4: new FormControl(null, Validators.required)
-    })
+      digit4: new FormControl(null, Validators.required),
+    });
   }
 
-  onVerifyOtp(){
-    if(this.otpForm.invalid){
-      return
+  onVerifyOtp() {
+    if (this.otpForm.invalid) {
+      return;
     }
-    this.verifySuccess = true
+    this.verifySuccess = true;
   }
 
-  singleDigit(control: string){
-    if(this.otpForm.get(control)?.value.length >= 2){
-      console.log(this.otpForm.get(control)?.value.slice(0,1))
-      this.otpForm.get(control)?.setValue(this.otpForm.get(control)?.value.slice(0,1))
-      this.otpForm.get(control)?.updateValueAndValidity()
+  singleDigit(control: string) {
+    if (this.otpForm.get(control)?.value.length >= 2) {
+      console.log(this.otpForm.get(control)?.value.slice(0, 1));
+      this.otpForm
+        .get(control)
+        ?.setValue(this.otpForm.get(control)?.value.slice(0, 1));
+      this.otpForm.get(control)?.updateValueAndValidity();
     }
   }
 
-  resendOtp(){
-    this.otpForm.reset()
+  resendOtp() {
+    this.otpForm.reset();
   }
 
-  modalEvent(event: boolean){
-    this._router.navigate(['/login'])
+  confirmModal(event: boolean) {
+    const frag = this._sarService.encodeParams({ ...this.fragment });
+    this._router.navigate(['/login'], { fragment: frag });
   }
 
-  back(){
-    this._router.navigate([''])
+  back() {
+    this._router.navigate(['']);
   }
-
 }
